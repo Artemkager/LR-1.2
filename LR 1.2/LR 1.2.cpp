921cace0c2541df20aa78903b3c4e99a1ce3dbbb
airex <airex@DESKTOP-PAB22CS>
2021-06-27 20:21:19 +0300

    Добавьте файлы проекта.

---
+++ LR 1.2/LR 1.2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void simvol(char a) {
+	do {
+		cout << "Введете символ" << endl;
+		cin >> a;
+	} while (!isalpha(a));
+}
+
+void stroka(string b, char a) {
+	do {
+		cout << "Введите строку" << endl;
+	} while (getline(cin, b));
+	for (int i = 0; i < b[i] != '\0'; i++) {
+		if (b[i] == a) {
+			cout << "Символ " << a << " стоит на - " << i + 1 << "месте";
+		}
+	}
+
+}
+
+int main()
+{
+	char a = '1';
+	string(b);
+	setlocale(LC_ALL, "RU");
+	simvol(a);
+	stroka(b, a);
+	return 0;
+	system("pause");
+}
